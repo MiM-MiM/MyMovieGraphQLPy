fb1363f39347f97a62634ca3a9c652435782a057
MiM-MiM <69122723+MiM-MiM@users.noreply.github.com>
2025-08-13 19:14:05 -0400

    Examples: Some test examples This is not a final test at all, it is to be used to show examples. The examples shown are basic for now and need expanded upon. They should have the ability to update data and showcase most attributes.

---
+++ tests/Example.GetByID.py
@@ -0,0 +1,26 @@
+from MyMovieGraphQL.GetByID import getByID
+if __name__ == "__main__":
+    # Example usage
+    movie_id = "tt1981677" # Pitch Perfect (2012)
+    show_id = "tt1553656" #  Under the Dome (2013-2015)
+    episode_id = "tt0606035" # House S01E01
+    name_id = "nm0908094" # Paul Walker
+    name_id2 = "nm0000206" # Keanu Reeves
+    
+    movie_data = getByID(movie_id)
+    print(movie_data)
+
+    show_data = getByID(show_id)
+    print(show_data)
+    
+    name_data = getByID(name_id)
+    print(name_data)
+
+    name_data2 = getByID(name_id2)
+    print(name_data2)
+
+    episode_data = getByID(episode_id)
+    try:
+        print(f"{episode_data.series} - {episode_data}") # Prints "House (2004) - S01E01 - Pilot (2004)"
+    except AttributeError:
+        print(episode_data)

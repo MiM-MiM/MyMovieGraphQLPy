717d3074b66eb15843e591f6e3e8524e5e21e294
MiM-MiM <69122723+MiM-MiM@users.noreply.github.com>
2025-08-24 19:36:04 -0400

    Feature: dunder strings - Start of __str__ methods - Moved regex data class to main init file. - Added a few specific cases
    \#Examples:
    Generated using this script (bio was trimmed):
    ```python
    from MyMovieGraphQL import GetByID
    movie = GetByID.getByID("tt0084787")
    print(movie)
    for key in movie.keys():
        print(f"\t{key}: {movie.get(key)}")
    
    show = GetByID.getByID("tt13617060")
    print(show)
    for key in show.keys():
        print(f"\t{key}: {show.get(key)}")
    
    name = GetByID.getByID("nm0000115")
    print(name)
    for key in name.keys():
        print(f"\t{key}: {name.get(key)}")
    ```
    \#\# The Thing (1982)
            __typename: Title
            canonicalUrl: https://www.imdb.com/title/tt0084787/
            certificate: R (United States)
            id: tt0084787
            originalTitleText: The Thing
            primaryImage: https://m.media-amazon.com/images/M/MV5BYTA3NDU5MWEtNTk4Yy00ZDNkLThmZTQtMjU3ZGVhYzAyMzU4XkEyXkFqcGc@._V1_.jpg
            productionStatus: Released
            ratingsSummary: 8.2 (506053 votes)
            releaseDate: June 25, 1982
            releaseYear: 1982
            runtime: 1h 49m
            spokenLanguages: English (United States)
            titleGenres: [Horror, Mystery, Sci-Fi]
            titleText: The Thing
            titleType: Movie
    
    \#\# History of Swear Words (2021-2021)
            __typename: Title
            canonicalUrl: https://www.imdb.com/title/tt13617060/
            certificate: TV-MA (United States)
            id: tt13617060
            originalTitleText: History of Swear Words
            primaryImage: https://m.media-amazon.com/images/M/MV5BZmMzNTk1MzgtYzg1NC00MTA0LWFiNzMtZDQ2MGY0N2M5ZDVhXkEyXkFqcGc@._V1_.jpg
            productionStatus: Released
            ratingsSummary: 6.4 (5736 votes)
            releaseDate: January 5, 2021
            releaseYear: 2021-2021
            runtime: None
            spokenLanguages: English (United States)
            titleGenres: [Documentary, Comedy]
            titleText: History of Swear Words
            titleType: TV Series
    
    \#\# Nicolas Cage (1964-01-07)
            __typename: Name
            akas: [Nicholas Cage, Nicolas Kim Coppola, Nicolas Coppola]
            bio: Nicolas Cage was born Nicolas Kim Coppola in Long Beach, California...
            birthDate: January 7, 1964
            birthLocation: Long Beach, California, USA
            birthName: Nicholas Kim Coppola
            canonicalUrl: https://www.imdb.com/name/nm0000115/
            deathCause: None
            deathDate: None
            deathLocation: None
            deathStatus: ALIVE
            height: 6′ (1.83 m)
            id: nm0000115
            nameText: Nicolas Cage

--- MyMovieGraphQL/GetByID.py
+++ MyMovieGraphQL/GetByID.py
@@ -1,19 +1,6 @@
-import re
-from dataclasses import dataclass
+from MyMovieGraphQL import GraphQL, MyMovie, regex_in
 
-from MyMovieGraphQL import GraphQL, MyMovie
-
-@dataclass
-class regex_in:
-    string: str
-
-    def __eq__(self, other: str | re.Pattern):  # type: ignore
-        if isinstance(other, str):
-            other = re.compile(other)
-        assert isinstance(other, re.Pattern)
-        return other.fullmatch(self.string) is not None
-
-def getByID(id: str) -> object:
+def getByID(id: str) -> MyMovie:
     query_name = ""
     match regex_in(id):
         case r'tt\d{7,}':
--- MyMovieGraphQL/__init__.py
+++ MyMovieGraphQL/__init__.py
@@ -1,4 +1,16 @@
 from typing import Iterable, Any
+import re
+from dataclasses import dataclass
+
+@dataclass
+class regex_in:
+    string: str
+
+    def __eq__(self, other: str | re.Pattern):  # type: ignore
+        if isinstance(other, str):
+            other = re.compile(other)
+        assert isinstance(other, re.Pattern)
+        return other.fullmatch(self.string) is not None
 
 class MyMovie:
     def __init__(self, obj: dict) -> None:
@@ -73,6 +85,133 @@ class MyMovie:
     def items(self):
         return self.data.items()
     
+    def __repr__(self) -> str:
+        if self.data.get('id'):
+            return f"<--- {self.ofType}: {self.__str__()} --->"
+        return self.__str__()
+    
+    def __str__(self) -> str:
+        match regex_in(self.ofType):
+            case 'Title':
+                return self._titleStr()
+            case 'Name':
+                return self._nameStr()
+            case 'YearRange':
+                return self._yearRangeStr()
+            case 'TitleGenre':
+                return self._titleGenreStr()
+            case 'Certificate':
+                return self._certificateStr()
+            case 'RatingsSummary':
+                return self._ratingsSummaryStr()
+            case r'.*Connection':
+                # Any connections we can return the string form of the list
+                return str(list(self.itterableAttribute()))
+            case _:
+                return self._otherStr()
+
+    def _certificateStr(self) -> str:
+        rating = self.data.get('rating')
+        country = self.data.get('country')
+        if rating is not None and country is not None:
+            return f"{rating} ({country})"
+        if rating is not None:
+            return str(rating)
+        return 'Unknown Rating'
+
+    def _connection(self) -> str:
+        attr = self.itterableAttribute()
+        if isinstance(attr, Iterable):
+            return str(list(attr))
+        return self.ofType
+
+    def _nameStr(self) -> str:
+        name = self.data.get('nameText')
+        birthDate = self.data.get('birthDate')
+        deathDate = self.data.get('deathDate')
+        deathStatus = self.data.get('deathStatus')
+        birthStr, deathStr = "", ""
+        if birthDate is not None:
+            birthStr = str(birthDate['date'])
+        if deathDate is not None:
+            deathStr = str(deathDate['date'])
+        nameString = str(name)
+        if deathStatus == 'ALIVE':
+            nameString = f"{name} ({birthStr})"
+        if deathStatus in ["DEAD", "PRESUMED_DEAD"]:
+            nameString = f"{name} ({birthStr} - {deathStr})"
+        return nameString
+
+    def _titleGenreStr(self):
+        genre = self.data.get('genre')
+        if isinstance(genre, type(self)):
+            genre = str(genre)
+        elif genre is None:
+            genre = ''
+        return genre
+    
+    def _ratingsSummaryStr(self) -> str:
+        aggregateRating = self.get('aggregateRating')
+        voteCount = self.get('voteCount')
+        aggregateRatingStr = 'Unknown'
+        if aggregateRating is not None:
+            aggregateRatingStr = str(aggregateRating)
+        if voteCount is not None:
+            aggregateRatingStr = f"{aggregateRatingStr} ({voteCount} votes)"
+        return aggregateRatingStr
+
+    def _yearRangeStr(self):
+        year: int | None = self.data.get('year', 0)
+        endYear: int | None = self.data.get('endYear', 0)
+        yearRangeStr: str = ""
+        if year:
+            yearRangeStr = f"{year}"
+        if endYear:
+            yearRangeStr = f"{yearRangeStr}-{endYear}"
+        return yearRangeStr
+
+    def _titleStr(self) -> str:
+        title = self.data.get('titleText')
+        year = self.data.get('releaseYear')
+        return f"{title} ({year})"
+
+    def _otherStr(self):
+        text = self.data.get('text')
+        url = self.data.get('url')
+        value = self.data.get('value')
+        plainText = self.data.get('plainText')
+        displayableProperty = self.data.get('displayableProperty')
+        if text is not None:
+            return str(text)
+        if url is not None:
+            return str(url)
+        if plainText is not None and isinstance(plainText, str):
+            return plainText
+        if value is not None:
+            return str(value)
+        current = ''
+        # Any that we want to us starts/ends/contains should go here.
+        for key in self.data.keys():
+            if self.data.get(key) is None:
+                continue
+            if key.startswith('current'):
+                current = str(self.data[key])
+            if key == 'language' and 'Language' in self.ofType:
+                return str(self.data.get('language'))
+        if current:
+            return current
+        if displayableProperty is not None:
+            return str(displayableProperty)
+        keys = [
+            key
+            for key in self.data.keys()
+            if not key.startswith('_')
+        ]
+        # Types with a single attribute should print that.
+        if len(keys) == 1:
+            return str(self.data.get(keys[0]))
+        return f"<--- {self.ofType}: {keys} --->"
+
     def __getitem__(self, val):
         if self.ofType.endswith('Connection') and isinstance(val, int):
             node = self.data.get('edges', [])[val].get('node')
@@ -111,5 +250,10 @@ class MyMovie:
 
     def itterableAttribute(self) -> Iterable | None:
         if self.ofType.endswith('Connection'):
-            return self.data.get('edges', [])
+            edges = [
+                edge.get('node')
+                for edge in self.data.get('edges', [])
+            ]
+            return iter(edges)
+
         return None
